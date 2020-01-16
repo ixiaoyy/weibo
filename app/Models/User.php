6efbb47c28b028457534eef4a303e9d59e800ba3
xiaoyz <phpxiaoyz@163.com>
2020-01-16 08:07:31 +0000

    Move user model to models folder

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,17 +1,23 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+// 授权相关功能的引用
 use Illuminate\Foundation\Auth\User as Authenticatable;
+
+// 消息推送
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use Notifiable;
 
+    protected $table = 'users';
+
     /**
-     * The attributes that are mass assignable.
+     * 只有包含在该属性中的字段才能够被正常更新
      *
      * @var array
      */
@@ -20,7 +26,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for arrays.
+     * 当我们需要对用户密码或其它敏感信息在用户实例通过数组或 JSON 显示时进行隐藏
      *
      * @var array
      */
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -7,30 +7,37 @@ use Illuminate\Support\Facades\Schema;
 class CreateUsersTable extends Migration
 {
     /**
-     * Run the migrations.
+     * 迁移数据-生成数据
      *
      * @return void
      */
     public function up()
     {
+        // Schema::create('users', function (Blueprint $table) { 建表 , 固定写法
         Schema::create('users', function (Blueprint $table) {
+            // 定义自增ID  $table->increments('id');
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
+            // timestamp 日期类型 格式： 2020-01-14 00:00:01  nullable() 可以为空，默认为空;
             $table->timestamp('email_verified_at')->nullable();
+            // $table->string('password', 60); 可增加第二个参数，限制长度
             $table->string('password');
+            // $table->rememberToken(); 固定写法为用户创建一个 remember_token 字段，用于保存『记住我』的相关信息。
             $table->rememberToken();
+            // $table->timestamps(); 固定写法，创建一个 created_at 和一个 updated_at 字段，分别用于保存创建时间和更新时间。
             $table->timestamps();
         });
     }
 
     /**
-     * Reverse the migrations.
+     * 回滚迁移
      *
      * @return void
      */
     public function down()
     {
+        // 回滚迁移
         Schema::dropIfExists('users');
     }
 }
