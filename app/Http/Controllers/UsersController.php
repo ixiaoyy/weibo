7f9cca4a91fd58b9bf7cda83b4a5d9b4e975c4e0
xiaoyz <phpxiaoyz@163.com>
2020-03-11 10:27:09 +0000

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\User;
+
 class UsersController extends Controller
 {
     // 用户注册
     public function create()
     {
         return view('users.create');
+    }
+
+    // 展示用户页面
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
     }
 }
